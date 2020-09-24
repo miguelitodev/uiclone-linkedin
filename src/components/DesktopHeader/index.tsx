727fa557f63aaf549c8dac99237e26cc4113ebfc
miguelrisquelme <miguelrisquelme@gmail.com>
2020-09-23 23:52:45 -0300

    Add some styled about DesktopHeader

--- src/components/DesktopHeader/index.tsx
+++ src/components/DesktopHeader/index.tsx
@@ -1,11 +1,43 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+    Container,
+    Wrapper,
+    LinkedinIcon,
+    SearchIcon,
+    HomeIcon,
+    NotificationsIcon,
+    ProfileCircle,
+    CareDownIcon,
+} from "./styles";
 
 const DesktopHeader: React.FC = () => {
     return (
         <Container>
-            <h1>DesktopHeader</h1>
+            <Wrapper>
+                <div className="left">
+                    <LinkedinIcon />
+                    <SearchInput placeholder="Pesquisar" />
+                </div>
+                <div className="right">
+                    <nav>
+                        <button className="active">
+                            <HomeIcon />
+                            <span>Inicio</span>
+                        </button>
+                        <button>
+                            <NotificationsIcon />
+                            <span>Notificações</span>
+                        </button>
+                        <button>
+                            <ProfileCircle src="https://github.com/miguelrisquelme.png" />
+                            <span>
+                                Eu <CareDownIcon />
+                            </span>
+                        </button>
+                    </nav>
+                </div>
+            </Wrapper>
         </Container>
     );
 };
--- src/components/DesktopHeader/styles.ts
+++ src/components/DesktopHeader/styles.ts
@@ -1,5 +1,37 @@
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
+import { GrLinkedin } from "react-icons/gr";
+import { AiFillHome, AiOutlineBell, AiFillCaretDown } from "react-icons/ai";
 
-export const Container = styled.div`
+export const Container = styled.div``;
 
+export const Wrapper = styled.div``;
+
+const generalIconCSS = css`
+    width: 24px;
+    height: 24px;
+`;
+
+export const LinkedinIcon = styled(GrLinkedin)`
+    ${generalIconCSS}
+`;
+
+export const SearchInput = styled.input``;
+
+export const HomeIcon = styled(AiFillHome)`
+    ${generalIconCSS}
+`;
+
+export const NotificationsIcon = styled(AiOutlineBell)`
+    ${generalIconCSS}
+`;
+
+export const ProfileCircle = styled.img`
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    border: solid 1px var(--color-icons);
+`;
+
+export const CareDownIcon = styled(AiFillCaretDown)`
+    ${generalIconCSS}
 `;
