432b9118fa67a4aa097dd5d0fb52783b3a9be810
miguelrisquelme <miguelrisquelme@gmail.com>
2020-09-26 21:22:32 -0300

    Change the person on Page

--- src/components/LeftColumn/ProfilePanel/index.tsx
+++ src/components/LeftColumn/ProfilePanel/index.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import Panel from '../../Panel';
+import Panel from "../../Panel";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 const ProfilePanel: React.FC = () => {
-  return (
-    <Panel>
-      <Container>
-        <div className="profile-cover"></div>
-        <img
-          src="https://github.com/guilhermerodz.png"
-          alt="Avatar"
-          className="profile-picture"
-        />
-        <h1>Guilherme Rodz</h1>
-        <h2>Software Engineer @ Rocketseat</h2>
+    return (
+        <Panel>
+            <Container>
+                <div className="profile-cover"></div>
+                <img
+                    src="https://github.com/miguelrisquelme.png"
+                    alt="Avatar"
+                    className="profile-picture"
+                />
+                <h1>Miguel Riquelme</h1>
+                <h2>Software Engineer</h2>
 
-        <div className="separator"></div>
+                <div className="separator"></div>
 
-        <div className="key-value">
-          <span className="key">Quem viu seu perfil</span>
-          <span className="value">1.558</span>
-        </div>
-        <div className="key-value">
-          <span className="key">Viram sua publicação</span>
-          <span className="value">388</span>
-        </div>
-      </Container>
-    </Panel>
-  );
+                <div className="key-value">
+                    <span className="key">Quem viu seu perfil</span>
+                    <span className="value">1.558</span>
+                </div>
+                <div className="key-value">
+                    <span className="key">Viram sua publicação</span>
+                    <span className="value">388</span>
+                </div>
+            </Container>
+        </Panel>
+    );
 };
 
 export default ProfilePanel;
